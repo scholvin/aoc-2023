02591b3c67e596986a8859fdbd46c068c7c14548
John Scholvin <john@scholvin.com>
2023-12-03 09:34:23 -0600

    day 1 done

--- src/week1.cpp
+++ src/week1.cpp
@@ -3,13 +3,52 @@
 
 namespace week1
 {
+    const std::string DIGITS { "0123456789" };
     long day01a()
     {
-        return -1;
+        std::ifstream infile("../data/day01.dat");
+        std::string line;
+        long sum = 0;
+        while (std::getline(infile, line))
+        {
+            long first = line[line.find_first_of(DIGITS)] - '0';
+            long last = line[line.find_last_of(DIGITS)] - '0';
+            sum += first * 10 + last;
+        }
+
+        return sum;
     }
 
     long day01b()
     {
-        return -1;
+        const std::vector<std::string> NAMES {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        std::ifstream infile("../data/day01.dat");
+        std::string line;
+        long sum = 0;
+        while (std::getline(infile, line))
+        {
+            size_t first = line.find_first_of(DIGITS);
+            size_t last = line.find_last_of(DIGITS);
+            int fv = first == std::string::npos ? -1 : line[first] - '0';
+            int lv = last == std::string::npos ? -1 : line[last] - '0';
+            for (int i = 0; i < 10; i++)
+            {
+                size_t ftmp = line.find(NAMES[i]);
+                size_t ltmp = line.rfind(NAMES[i]);
+                if (ftmp != std::string::npos && ftmp < first)
+                {
+                    fv = i;
+                    first = ftmp;
+                }
+                if (ltmp != std::string::npos && ltmp > last)
+                {
+                    lv = i;
+                    last = ltmp;
+                }
+            }
+            sum += fv * 10 + lv;
+        }
+        
+        return sum;
     }
 };
