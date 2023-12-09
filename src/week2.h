aae19c0d705fc89ba3af66337bd675cdea662b9a
John Scholvin <john@scholvin.com>
2023-12-09 07:06:57 -0600

    wrong name, will fix

--- src/week2.h
+++ src/week2.h
@@ -5,10 +5,12 @@
 namespace week2
 {
     long day08(char part);
+    long day09a()
 
     const runner::method_map_t method_map =
     {
         { "8a", std::bind(&day08, 'a') },
         { "8b", std::bind(&day08, 'b') },
+        { "9a", &day09a },
     };
 };
